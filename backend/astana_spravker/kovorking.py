d9aefb7f0ab3c31cf93f78ffd89ae2dce4d72c58
YernarT <toktaryernar@gmail.com>
2022-04-23 15:33:25 +0600

    rename: :pencil2: data -> astana_spravker

---
+++ backend/astana_spravker/kovorking.py
@@ -0,0 +1,16 @@
+import requests
+
+from . import BASE_URL
+from .scraping import scraping
+
+
+def get_data(page: int):
+    url = f'{BASE_URL}/kovorking/page-{page}/'
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return False, {'message': 'nonexistent page'}
+
+    result_set = scraping(response.text)
+
+    return True, {'data': result_set, 'data_count': len(result_set), 'url': url}
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,15 @@ app = FastAPI()
 
 @app.get("/scraping/kovorking/{page}/")
 def kovorking(page):
-    from data.kovorking import get_data
+    from astana_spravker.kovorking import get_data
+    is_success, data_or_reason = get_data(page)
+
+    return Response(json.dumps(data_or_reason), 200 if is_success else 400)
+
+
+@app.get("/scraping/shop/{page}/")
+def shop(page):
+    from astana_spravker.shop import get_data
     is_success, data_or_reason = get_data(page)
 
     return Response(json.dumps(data_or_reason), 200 if is_success else 400)
