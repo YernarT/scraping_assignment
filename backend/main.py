0c4e3db718dfff595edc9877425e07a9af19f8d7
YernarT <toktaryernar@gmail.com>
2022-04-25 17:43:13 +0600

    update: :hammer: 添加 CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 
 @app.get("/scraping/kovorking/{page}/")
