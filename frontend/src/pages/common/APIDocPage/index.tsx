218edd958aeaf12f896cfd9ca3126eedd383543d
YernarT <toktaryernar@gmail.com>
2022-04-25 10:50:20 +0600

    update: :hammer: API Doc 文本

--- frontend/src/pages/common/APIDocPage/index.tsx
+++ frontend/src/pages/common/APIDocPage/index.tsx
@@ -1,5 +1,36 @@
+import { Typography, Divider } from 'antd';
+import { ApiBox } from '@/components/common';
 import { APIDocStyledBox } from './style';
 
+const { Title, Paragraph, Text, Link } = Typography;
+
 export default function APIDocPage() {
-	return <APIDocStyledBox>API Doc</APIDocStyledBox>;
+	return (
+		<APIDocStyledBox>
+			<Typography>
+				<Title>Scraping API документация</Title>
+				<Paragraph>
+					Отдел разработки: AstanaHub (компания Yona), этот{' '}
+					<Text code>scraping api</Text>
+					предназначен только для обучения.
+				</Paragraph>
+
+				<Divider />
+
+				<Title level={2}>Справочник организаций в Нур-Султане</Title>
+				<Paragraph>
+					<Text code>BASE_URL</Text>:{' '}
+					<Link href="https://astana.spravker.ru">
+						https://astana.spravker.ru
+					</Link>
+				</Paragraph>
+				<Paragraph>
+					Справочник Нур-Султана, услуги Нур-Султана, скидки в Нур-Султане,
+					вакансии в Нур-Султане, отзывы об организациях.
+				</Paragraph>
+
+				<ApiBox />
+			</Typography>
+		</APIDocStyledBox>
+	);
 }
